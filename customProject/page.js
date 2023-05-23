60df87cf21055e1e8ba178707f41ad962634c314
preetham-saldanha <preethamsaldanha26@gmail.com>
2023-05-23 17:52:35 +0530

    solved double click and failure of removeEventListener bug

--- customProject/page.js
+++ customProject/page.js
@@ -1,114 +1,92 @@
-const first_card= document.getElementById("first-card");
-const second_card= document.getElementById("second-card");
-const hidden_content = document.querySelector(".hidden-content")
-const top_layer = document.querySelector(".top-layer")
-const first_shell = document.getElementById("first-shell")
-const second_shell= document.getElementById("second-shell")
-const third_shell = document.getElementById("third-shell")
+const first_card = document.getElementById("first-card");
+const second_card = document.getElementById("second-card");
+const hidden_content = document.querySelector(".hidden-content");
+const top_layer = document.querySelector(".top-layer");
+const first_shell = document.getElementById("first-shell");
+const second_shell = document.getElementById("second-shell");
+const third_shell = document.getElementById("third-shell");
 
 // =======================first card event listener==========================
 
+function first_card_handler() {
+  first_shell.childNodes[0].addEventListener("click", () => {
+    // first_card.removeAttribute("change-first-card")
 
-first_card.addEventListener("click",()=>{
+    setTimeout(() => {
+      top_layer.classList.remove("tiny-fade");
+      first_card.addEventListener("click", first_card_handler);
+    }, 800);
 
+    setTimeout(() => {
+      first_shell.classList.remove("zoom-to-full", "shrink-back");
 
-    first_shell.childNodes[0].addEventListener("click",()=>{
-        // first_card.removeAttribute("change-first-card")
+      second_shell.classList.remove("do-not-display");
+      third_shell.classList.remove("do-not-display");
+    }, 1400);
 
-        setTimeout(()=>{
-            top_layer.classList.remove("tiny-fade")
-        },800)  
+    first_shell.childNodes[0].removeEventListener("click", first_card_handler); // why is this working?...second argument is not its own handler rather is parent's hanlder
+    console.log("cheking");
+    first_shell.classList.add("shrink-back");
+    top_layer.classList.remove("do-not-display");
+  });
 
-       setTimeout(()=>{
-       first_shell.classList.remove("zoom-to-full", "shrink-back")
+  // first_card.setAttribute("change-first-card",true);
 
-       second_shell.classList.remove("do-not-display")
-       third_shell.classList.remove("do-not-display")
+  first_shell.classList.add("zoom-to-full");
 
+  first_card.removeEventListener("click", first_card_handler);
 
-
-       first_shell.childNodes[0].removeEventListener("click")
-    
-       },1400)
-
-   
-
-first_shell.classList.add("shrink-back")
-top_layer.classList.remove( "do-not-display")
-
-
-    })
-
-
-        // first_card.setAttribute("change-first-card",true);
-
-
-        first_shell.classList.add("zoom-to-full")
-
-
-
-        second_shell.classList.add("do-not-display")
-        third_shell.classList.add("do-not-display")
-        top_layer.classList.add("tiny-fade")
-        setTimeout(()=>{
-        top_layer.classList.add("do-not-display")
-        }, 1000)
+  second_shell.classList.add("do-not-display");
+  third_shell.classList.add("do-not-display");
+  top_layer.classList.add("tiny-fade");
+  setTimeout(() => {
+    top_layer.classList.add("do-not-display");
+  }, 1000);
 }
-)
-
-
 
 //===============================second card event listener=================================
 
-
-second_card.addEventListener("click",(e)=>{
-
-
-
-second_shell.childNodes[0].addEventListener("click",()=>{
-
+function second_card_handler() {
+  console.log("parent 2nd card");
+  second_shell.childNodes[0].addEventListener("click", () => {
     // second_card.removeAttribute("change-first-card")
-    
-    setTimeout(()=>{
-        top_layer.classList.remove("tiny-fade")
-    },800)  
+    console.log("sidebar of 2nd shell");
+    // second_shell.childNodes[0].removeEventListener("click")
+    setTimeout(() => {
+      top_layer.classList.remove("tiny-fade");
+      second_card.addEventListener("click", second_card_handler);
+    }, 800);
 
-    setTimeout(()=>{
-        second_shell.classList.remove("zoom-to-full","shrink-back")
+    setTimeout(() => {
+      second_shell.classList.remove("zoom-to-full", "shrink-back");
 
-        
-        first_shell.classList.remove("do-not-display")
-        third_shell.classList.remove("do-not-display")
-        
-        
-    
-        second_shell.childNodes[0].removeEventListener("click")
-    },1400)
+      first_shell.classList.remove("do-not-display");
+      third_shell.classList.remove("do-not-display");
+    }, 1400);
 
+    second_shell.childNodes[0].removeEventListener(
+      "click",
+      second_card_handler
+    );
 
-    second_shell.classList.add("shrink-back")
+    second_shell.classList.add("shrink-back");
     top_layer.classList.remove("do-not-display");
-    
-})
+  });
 
+  // second_card.setAttribute("change-first-card",true);
 
-    // second_card.setAttribute("change-first-card",true);
+  second_shell.classList.add("zoom-to-full");
+  second_card.removeEventListener("click", second_card_handler);
+  first_shell.classList.add("do-not-display");
+  third_shell.classList.add("do-not-display");
+  top_layer.classList.add("tiny-fade");
+  setTimeout(() => {
+    top_layer.classList.add("do-not-display");
+  }, 1000);
+}
 
+//================================================================================
 
-    second_shell.classList.add("zoom-to-full")
-    
-    
-    
-    first_shell.classList.add("do-not-display")
-    third_shell.classList.add("do-not-display")
-    top_layer.classList.add("tiny-fade")
-        setTimeout(()=>{
-            top_layer.classList.add("do-not-display")
-        }, 1000)
-    })
+first_card.addEventListener("click", first_card_handler);
 
-
-
-
-
-
+second_card.addEventListener("click", second_card_handler);
